b6eb5c84bbd22dacb7c1278ceeb955e572e40fe5
Thumpudi Vishnu <87056972+vishnuthumpudi@users.noreply.github.com>
2024-11-16 19:17:48 +0530

    Added Fun to Tasks

---
+++ src/components/Todo.jsx
@@ -0,0 +1,61 @@
+import React, { useRef, useState } from 'react'
+import todo_icon from '../assets/todo_icon.png'
+import TasksItems from './TasksItems'
+
+const Todo = () => {
+    const [todoList, setTodoList] = useState([]);
+
+    const inputRef = useRef()
+
+    const add = () => {
+        const inputText = inputRef.current.value.trim();
+
+        if(inputText === ""){
+            return null;
+        }
+
+        const newTodo = {
+            id : Date.now(),
+            text : inputText,
+            isComplete : false,
+        }
+        setTodoList((prev) => [...prev, newTodo]);
+        inputRef.current.value = "";
+    }
+
+    const deletetask = (id) => {
+        setTodoList((prevtodo)=>{
+            return prevtodo.filter((todo)=>{
+                todo.id !== id
+            })
+        })
+    }
+  
+  
+    return (
+    <div className = 'bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+        {/* --- title ---- */}
+        <div className = 'flex items-center mt-7 gap-2'>
+            <img className = 'w-8' src={todo_icon} alt=""/>
+            <h1 className = 'text-3xl font-semibold'>Tasks List</h1>
+        </div>
+
+        {/* --- input box ---- */}
+
+        <div className = 'flex items-center my-7 bg-gray-200 rounded-full'>
+            <input ref = {inputRef} className = 'bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder: text-slate-600' type = "text" placeholder='Add Your Task'/>
+            <button onClick = {add} className = 'border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Add +</button>
+        </div>
+
+        {/* --- tasks list ---- */}
+
+        <div>
+            {todoList.map((item,index)=>{
+                return <TasksItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deletetask={deletetask}/>
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default Todo
